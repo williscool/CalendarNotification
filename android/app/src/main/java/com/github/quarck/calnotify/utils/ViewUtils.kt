f07023d393f5c36b6162f579afb1a7a48c2634e6
William Harris <wharris@upscalews.com>
2023-10-09 04:42:16 +0000

    close to running!

--- android/app/src/main/java/com/github/quarck/calnotify/utils/ViewUtils.kt
+++ android/app/src/main/java/com/github/quarck/calnotify/utils/ViewUtils.kt
@@ -31,8 +31,8 @@ fun <T : View?> Activity.find(id: Int): T? = findViewById(id) as T?
 fun <T : View?> Fragment.find(id: Int): T? = view?.findViewById(id) as T?
 
 
-fun <T : View?> View.findOrThrow(id: Int): T = (findViewById(id) ?: throw Exception("Cant find resource id $id"))  as T
+fun <T : View?> View.findOrThrow(id: Int): T = (findViewById(id) as T?: throw Exception("Cant find resource id $id"))
 
-fun <T : View?> Activity.findOrThrow(id: Int): T = (findViewById(id) ?: throw Exception("Cant find resource id $id"))  as T
+fun <T : View?> Activity.findOrThrow(id: Int): T = (findViewById(id) as T ?: throw Exception("Cant find resource id $id"))
 
-fun <T : View?> Fragment.findOrThrow(id: Int): T = (view?.findViewById(id) ?: throw Exception("Cant find resource id $id")) as T
+fun <T : View?> Fragment.findOrThrow(id: Int): T = (view?.findViewById(id) as T ?: throw Exception("Cant find resource id $id"))
