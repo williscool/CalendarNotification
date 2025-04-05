f4c5c19efadc62ccc55c608ad98541648ff2806a
William Harris <wharris@upscalews.com>
2025-04-05 15:40:18 +0000

    revert: just make clock do what it always has for now
    we can make it more robust in another pr more focused on it

--- android/app/src/main/java/com/github/quarck/calnotify/utils/CNPlusSystemClock.kt
+++ android/app/src/main/java/com/github/quarck/calnotify/utils/CNPlusSystemClock.kt
@@ -13,12 +13,19 @@ class CNPlusSystemClock : CNPlusClockInterface {
     
     override fun currentTimeMillis(): Long = clock.millis()
     
+    /**
+     * Sleep for the given number of milliseconds
+     * 
+     * TODO: Implement a more robust sleep mechanism
+     * for now we just want to preserve the behavior of the original code
+     * @param millis the number of milliseconds to sleep
+     */
     override fun sleep(millis: Long) {
-        try {
-            Thread.sleep(millis)
-        } catch (e: InterruptedException) {
-            Thread.currentThread().interrupt()
-        }
+        // try {
+        Thread.sleep(millis)
+        // } catch (e: InterruptedException) {
+        //     Thread.currentThread().interrupt()
+        // }
     }
     
     override fun underlying(): Clock = clock
