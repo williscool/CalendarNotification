e8c1a18000fb1ba7b8ecf534e272da033a089895
Will <williscool@users.noreply.github.com>
2025-04-24 22:57:25 +0000

    ci: extract setup_dev_container_android_toolchain
    to its own script

---
+++ scripts/setup_dev_container_android_toolchain.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Setup script for devcontainer aka GitHub Codespace android development environment
+
+## begin ephemeral android sdk setup. will have to do this every time.
+echo 'export ANDROID_HOME=/tmp/Android/Sdk' >> ~/.bashrc
+echo 'export PATH=$PATH:$ANDROID_HOME/cmdline-tools/latest/bin' >> ~/.bashrc
+echo 'export PATH=$PATH:$ANDROID_HOME/platform-tools' >> ~/.bashrc
+echo 'export PATH=$ANDROID_HOME/cmdline-tools/latest/bin:$ANDROID_HOME/emulator:$ANDROID_HOME/platform-tools:$PATH' >> ~/.bashrc
+echo 'export ANDROID_AVD_HOME=/tmp/my_avd_home' >> ~/.bashrc
+
+# why becuase they give you hella temp space but a small of permanent 
+wget https://dl.google.com/android/repository/commandlinetools-linux-13114758_latest.zip 
+mv commandlinetools-linux-13114758_latest.zip /tmp/
+mkdir -p $ANDROID_HOME
+unzip /tmp/commandlinetools-linux-13114758_latest.zip -d $ANDROID_HOME
+mkdir -p $ANDROID_HOME/cmdline-tools/
+mkdir -p $ANDROID_HOME/cmdline-tools/latest/
+mkdir -p $ANDROID_AVD_HOME
+mv $ANDROID_HOME/cmdline-tools/* $ANDROID_HOME/cmdline-tools/latest/
+
+yes | sdkmanager --sdk_root=${ANDROID_HOME} "platform-tools" "platforms;android-34" "build-tools;34.0.0"
+yes | sdkmanager --install "system-images;android-34;google_apis_playstore;x86_64"
+
+mkdir -p $ANDROID_AVD_HOME
+
+sudo apt install qemu-kvm
+sudo groupadd -r kvm
+sudo adduser $USER kvm
+sudo chown $USER /dev/kvm
+
+avdmanager create avd --name 7.6_Fold-in_with_outer_display_API_34 --package "system-images;android-34;google_apis_playstore;x86_64" --device "7.6in Foldable"
+# you can run with  emulator -avd 7.6_Fold-in_with_outer_display_API_34 -no-window -gpu swiftshader_indirect -noaudio -no-boot-anim -camera-back none -no-snapshot
--- scripts/setup_github_codespace.sh
+++ scripts/setup_github_codespace.sh
@@ -19,37 +19,8 @@ echo 'esac' >> ~/.bashrc
 echo '' >> ~/.bashrc
 echo 'eval "$(mcfly init bash)"' >> ~/.bashrc
 
-echo 'export ANDROID_HOME=/tmp/Android/Sdk' >> ~/.bashrc
-echo 'export PATH=$PATH:$ANDROID_HOME/cmdline-tools/latest/bin' >> ~/.bashrc
-echo 'export PATH=$PATH:$ANDROID_HOME/platform-tools' >> ~/.bashrc
-echo 'export PATH=$ANDROID_HOME/cmdline-tools/latest/bin:$ANDROID_HOME/emulator:$ANDROID_HOME/platform-tools:$PATH' >> ~/.bashrc
-echo 'export ANDROID_AVD_HOME=/tmp/my_avd_home' >> ~/.bashrc
-
 ## begin ephemeral android sdk setup. will have to do this every time.
-# why becuase they give you hella temp space but a small of permanent 
-wget https://dl.google.com/android/repository/commandlinetools-linux-13114758_latest.zip 
-mv commandlinetools-linux-13114758_latest.zip /tmp/
-mkdir -p $ANDROID_HOME
-unzip /tmp/commandlinetools-linux-13114758_latest.zip -d $ANDROID_HOME
-mkdir -p $ANDROID_HOME/cmdline-tools/
-mkdir -p $ANDROID_HOME/cmdline-tools/latest/
-mkdir -p $ANDROID_AVD_HOME
-mv $ANDROID_HOME/cmdline-tools/* $ANDROID_HOME/cmdline-tools/latest/
-
-yes | sdkmanager --sdk_root=${ANDROID_HOME} "platform-tools" "platforms;android-34" "build-tools;34.0.0"
-yes | sdkmanager --install "system-images;android-34;google_apis_playstore;x86_64"
-
-sudo apt install qemu-kvm
-sudo groupadd -r kvm
-sudo adduser $USER kvm
-sudo chown $USER /dev/kvm
-
-
-avdmanager create avd --name 7.6_Fold-in_with_outer_display_API_34 --package "system-images;android-34;google_apis_playstore;x86_64" --device "7.6in Foldable"
-# you can run with  emulator -avd 7.6_Fold-in_with_outer_display_API_34 -no-window -gpu swiftshader_indirect -noaudio -no-boot-anim -camera-back none -no-snapshot
-
-
-
+./scripts/setup_dev_container_android_toolchain.sh
 # end ephemeral android sdk setup
 
 # begin ephemeral gradle setup
@@ -57,8 +28,8 @@ export GRADLE_USER_HOME=/tmp/gradle-cache
 mkdir -p $GRADLE_USER_HOME
 # end ephemeral gradle setup
 
-
 # Setup Git configuration
+# username and email are setup by github
 echo "Setting up Git configuration..."
 git config --global color.ui true
 git config --global color.branch true
