36083b9b53a6e4d47a152e9e6e6f4517731b16af
William Harris <wharris@upscalews.com>
2025-12-03 05:12:03 +0000

    test: broadcastreceivers

--- android/app/src/androidTest/java/com/github/quarck/calnotify/broadcastreceivers/BroadcastReceiverTest.kt
+++ android/app/src/androidTest/java/com/github/quarck/calnotify/broadcastreceivers/BroadcastReceiverTest.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import com.github.quarck.calnotify.app.ApplicationController
+import com.github.quarck.calnotify.calendarmonitor.CalendarMonitorInterface
 import com.github.quarck.calnotify.logs.DevLog
 import io.mockk.*
 import org.junit.After
@@ -20,16 +21,22 @@ class BroadcastReceiverTest {
     private val LOG_TAG = "BroadcastReceiverTest"
 
     private lateinit var context: Context
+    private lateinit var mockCalendarMonitor: CalendarMonitorInterface
 
     @Before
     fun setup() {
         DevLog.info(LOG_TAG, "Setting up BroadcastReceiverTest")
         context = InstrumentationRegistry.getInstrumentation().targetContext
+        mockCalendarMonitor = mockk(relaxed = true)
 
         // Mock ApplicationController to verify calls
         mockkObject(ApplicationController)
         every { ApplicationController.onBootComplete(any()) } just Runs
         every { ApplicationController.onEventAlarm(any()) } just Runs
+        every { ApplicationController.onAppUpdated(any()) } just Runs
+        every { ApplicationController.onTimeChanged(any()) } just Runs
+        every { ApplicationController.onReceivedRescheduleConfirmations(any(), any()) } just Runs
+        every { ApplicationController.CalendarMonitor } returns mockCalendarMonitor
     }
 
     @After
@@ -143,6 +150,166 @@ class BroadcastReceiverTest {
 
         val receiver = ReminderExactAlarmBroadcastReceiver()
         assert(receiver is android.content.BroadcastReceiver)
+    }
+
+    // === AppUpdatedBroadcastReceiver tests ===
+
+    @Test
+    fun testAppUpdatedBroadcastReceiverCallsOnAppUpdated() {
+        DevLog.info(LOG_TAG, "Running testAppUpdatedBroadcastReceiverCallsOnAppUpdated")
+
+        val receiver = AppUpdatedBroadcastReceiver()
+        val intent = Intent(Intent.ACTION_MY_PACKAGE_REPLACED)
+
+        receiver.onReceive(context, intent)
+
+        verify(exactly = 1) { ApplicationController.onAppUpdated(context) }
+    }
+
+    @Test
+    fun testAppUpdatedBroadcastReceiverWithNullContext() {
+        DevLog.info(LOG_TAG, "Running testAppUpdatedBroadcastReceiverWithNullContext")
+
+        val receiver = AppUpdatedBroadcastReceiver()
+        val intent = Intent()
+
+        receiver.onReceive(null, intent)
+
+        verify(exactly = 0) { ApplicationController.onAppUpdated(any()) }
+    }
+
+    // === TimeSetBroadcastReceiver tests ===
+
+    @Test
+    fun testTimeSetBroadcastReceiverCallsOnTimeChanged() {
+        DevLog.info(LOG_TAG, "Running testTimeSetBroadcastReceiverCallsOnTimeChanged")
+
+        val receiver = TimeSetBroadcastReceiver()
+        val intent = Intent(Intent.ACTION_TIME_CHANGED)
+
+        receiver.onReceive(context, intent)
+
+        verify(exactly = 1) { ApplicationController.onTimeChanged(context) }
+    }
+
+    @Test
+    fun testTimeSetBroadcastReceiverWithNullContext() {
+        DevLog.info(LOG_TAG, "Running testTimeSetBroadcastReceiverWithNullContext")
+
+        val receiver = TimeSetBroadcastReceiver()
+        val intent = Intent()
+
+        receiver.onReceive(null, intent)
+
+        verify(exactly = 0) { ApplicationController.onTimeChanged(any()) }
+    }
+
+    // === ManualEventAlarmBroadcastReceiver tests ===
+
+    @Test
+    fun testManualEventAlarmBroadcastReceiverCallsCalendarMonitor() {
+        DevLog.info(LOG_TAG, "Running testManualEventAlarmBroadcastReceiverCallsCalendarMonitor")
+
+        val receiver = ManualEventAlarmBroadcastReceiver()
+        val intent = Intent()
+
+        receiver.onReceive(context, intent)
+
+        verify(exactly = 1) { mockCalendarMonitor.onAlarmBroadcast(context, intent) }
+    }
+
+    @Test
+    fun testManualEventAlarmBroadcastReceiverWithNullContext() {
+        DevLog.info(LOG_TAG, "Running testManualEventAlarmBroadcastReceiverWithNullContext")
+
+        val receiver = ManualEventAlarmBroadcastReceiver()
+        val intent = Intent()
+
+        receiver.onReceive(null, intent)
+
+        verify(exactly = 0) { mockCalendarMonitor.onAlarmBroadcast(any(), any()) }
+    }
+
+    // === ManualEventExactAlarmBroadcastReceiver tests ===
+
+    @Test
+    fun testManualEventExactAlarmBroadcastReceiverCallsCalendarMonitor() {
+        DevLog.info(LOG_TAG, "Running testManualEventExactAlarmBroadcastReceiverCallsCalendarMonitor")
+
+        val receiver = ManualEventExactAlarmBroadcastReceiver()
+        val intent = Intent()
+
+        receiver.onReceive(context, intent)
+
+        verify(exactly = 1) { mockCalendarMonitor.onAlarmBroadcast(context, intent) }
+    }
+
+    // === ManualEventAlarmPeriodicRescanBroadcastReceiver tests ===
+
+    @Test
+    fun testManualEventAlarmPeriodicRescanBroadcastReceiverCallsCalendarMonitor() {
+        DevLog.info(LOG_TAG, "Running testManualEventAlarmPeriodicRescanBroadcastReceiverCallsCalendarMonitor")
+
+        val receiver = ManualEventAlarmPeriodicRescanBroadcastReceiver()
+        val intent = Intent()
+
+        receiver.onReceive(context, intent)
+
+        verify(exactly = 1) { mockCalendarMonitor.onPeriodicRescanBroadcast(context, intent) }
+    }
+
+    @Test
+    fun testManualEventAlarmPeriodicRescanBroadcastReceiverWithNullContext() {
+        DevLog.info(LOG_TAG, "Running testManualEventAlarmPeriodicRescanBroadcastReceiverWithNullContext")
+
+        val receiver = ManualEventAlarmPeriodicRescanBroadcastReceiver()
+        val intent = Intent()
+
+        receiver.onReceive(null, intent)
+
+        verify(exactly = 0) { mockCalendarMonitor.onPeriodicRescanBroadcast(any(), any()) }
+    }
+
+    // === RescheduleConfirmationsBroadcastReceiver tests ===
+
+    @Test
+    fun testRescheduleConfirmationsBroadcastReceiverCallsApplicationController() {
+        DevLog.info(LOG_TAG, "Running testRescheduleConfirmationsBroadcastReceiverCallsApplicationController")
+
+        val receiver = RescheduleConfirmationsBroadcastReceiver()
+        val intent = Intent().apply {
+            putExtra("reschedule_confirmations", "test_value")
+        }
+
+        receiver.onReceive(context, intent)
+
+        verify(exactly = 1) { ApplicationController.onReceivedRescheduleConfirmations(context, "test_value") }
+    }
+
+    @Test
+    fun testRescheduleConfirmationsBroadcastReceiverWithNullContext() {
+        DevLog.info(LOG_TAG, "Running testRescheduleConfirmationsBroadcastReceiverWithNullContext")
+
+        val receiver = RescheduleConfirmationsBroadcastReceiver()
+        val intent = Intent().apply {
+            putExtra("reschedule_confirmations", "test_value")
+        }
+
+        receiver.onReceive(null, intent)
+
+        verify(exactly = 0) { ApplicationController.onReceivedRescheduleConfirmations(any(), any()) }
+    }
+
+    @Test
+    fun testRescheduleConfirmationsBroadcastReceiverWithMissingExtra() {
+        DevLog.info(LOG_TAG, "Running testRescheduleConfirmationsBroadcastReceiverWithMissingExtra")
+
+        val receiver = RescheduleConfirmationsBroadcastReceiver()
+        val intent = Intent() // No extra
+
+        receiver.onReceive(context, intent)
+
+        verify(exactly = 0) { ApplicationController.onReceivedRescheduleConfirmations(any(), any()) }
     }
 }
 
--- android/app/src/test/java/com/github/quarck/calnotify/broadcastreceivers/BroadcastReceiverRobolectricTest.kt
+++ android/app/src/test/java/com/github/quarck/calnotify/broadcastreceivers/BroadcastReceiverRobolectricTest.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.test.core.app.ApplicationProvider
 import com.github.quarck.calnotify.app.ApplicationController
+import com.github.quarck.calnotify.calendarmonitor.CalendarMonitorInterface
 import io.mockk.*
 import org.junit.After
 import org.junit.Before
@@ -20,15 +21,21 @@ import org.robolectric.annotation.Config
 class BroadcastReceiverRobolectricTest {
 
     private lateinit var context: Context
+    private lateinit var mockCalendarMonitor: CalendarMonitorInterface
 
     @Before
     fun setup() {
         context = ApplicationProvider.getApplicationContext()
+        mockCalendarMonitor = mockk(relaxed = true)
 
         // Mock ApplicationController to verify calls
         mockkObject(ApplicationController)
         every { ApplicationController.onBootComplete(any()) } just Runs
         every { ApplicationController.onEventAlarm(any()) } just Runs
+        every { ApplicationController.onAppUpdated(any()) } just Runs
+        every { ApplicationController.onTimeChanged(any()) } just Runs
+        every { ApplicationController.onReceivedRescheduleConfirmations(any(), any()) } just Runs
+        every { ApplicationController.CalendarMonitor } returns mockCalendarMonitor
     }
 
     @After
@@ -148,6 +155,202 @@ class BroadcastReceiverRobolectricTest {
     fun testReminderExactAlarmBroadcastReceiverInstantiation() {
         val receiver = ReminderExactAlarmBroadcastReceiver()
         assert(receiver is android.content.BroadcastReceiver)
+    }
+
+    // === AppUpdatedBroadcastReceiver tests ===
+
+    @Test
+    fun testAppUpdatedBroadcastReceiverCallsOnAppUpdated() {
+        // Given
+        val receiver = AppUpdatedBroadcastReceiver()
+        val intent = Intent(Intent.ACTION_MY_PACKAGE_REPLACED)
+
+        // When
+        receiver.onReceive(context, intent)
+
+        // Then
+        verify(exactly = 1) { ApplicationController.onAppUpdated(context) }
+    }
+
+    @Test
+    fun testAppUpdatedBroadcastReceiverWithNullContext() {
+        // Given
+        val receiver = AppUpdatedBroadcastReceiver()
+        val intent = Intent()
+
+        // When - null context
+        receiver.onReceive(null, intent)
+
+        // Then - should not call ApplicationController
+        verify(exactly = 0) { ApplicationController.onAppUpdated(any()) }
+    }
+
+    // === TimeSetBroadcastReceiver tests ===
+
+    @Test
+    fun testTimeSetBroadcastReceiverCallsOnTimeChanged() {
+        // Given
+        val receiver = TimeSetBroadcastReceiver()
+        val intent = Intent(Intent.ACTION_TIME_CHANGED)
+
+        // When
+        receiver.onReceive(context, intent)
+
+        // Then
+        verify(exactly = 1) { ApplicationController.onTimeChanged(context) }
+    }
+
+    @Test
+    fun testTimeSetBroadcastReceiverWithNullContext() {
+        // Given
+        val receiver = TimeSetBroadcastReceiver()
+        val intent = Intent()
+
+        // When - null context
+        receiver.onReceive(null, intent)
+
+        // Then - should not call ApplicationController
+        verify(exactly = 0) { ApplicationController.onTimeChanged(any()) }
+    }
+
+    // === ManualEventAlarmBroadcastReceiver tests ===
+
+    @Test
+    fun testManualEventAlarmBroadcastReceiverCallsCalendarMonitor() {
+        // Given
+        val receiver = ManualEventAlarmBroadcastReceiver()
+        val intent = Intent()
+
+        // When
+        receiver.onReceive(context, intent)
+
+        // Then
+        verify(exactly = 1) { mockCalendarMonitor.onAlarmBroadcast(context, intent) }
+    }
+
+    @Test
+    fun testManualEventAlarmBroadcastReceiverWithNullContext() {
+        // Given
+        val receiver = ManualEventAlarmBroadcastReceiver()
+        val intent = Intent()
+
+        // When - null context
+        receiver.onReceive(null, intent)
+
+        // Then - should not call CalendarMonitor
+        verify(exactly = 0) { mockCalendarMonitor.onAlarmBroadcast(any(), any()) }
+    }
+
+    @Test
+    fun testManualEventAlarmBroadcastReceiverWithNullIntent() {
+        // Given
+        val receiver = ManualEventAlarmBroadcastReceiver()
+
+        // When - null intent
+        receiver.onReceive(context, null)
+
+        // Then - should not call CalendarMonitor
+        verify(exactly = 0) { mockCalendarMonitor.onAlarmBroadcast(any(), any()) }
+    }
+
+    // === ManualEventExactAlarmBroadcastReceiver tests ===
+
+    @Test
+    fun testManualEventExactAlarmBroadcastReceiverCallsCalendarMonitor() {
+        // Given
+        val receiver = ManualEventExactAlarmBroadcastReceiver()
+        val intent = Intent()
+
+        // When
+        receiver.onReceive(context, intent)
+
+        // Then
+        verify(exactly = 1) { mockCalendarMonitor.onAlarmBroadcast(context, intent) }
+    }
+
+    // === ManualEventAlarmPeriodicRescanBroadcastReceiver tests ===
+
+    @Test
+    fun testManualEventAlarmPeriodicRescanBroadcastReceiverCallsCalendarMonitor() {
+        // Given
+        val receiver = ManualEventAlarmPeriodicRescanBroadcastReceiver()
+        val intent = Intent()
+
+        // When
+        receiver.onReceive(context, intent)
+
+        // Then
+        verify(exactly = 1) { mockCalendarMonitor.onPeriodicRescanBroadcast(context, intent) }
+    }
+
+    @Test
+    fun testManualEventAlarmPeriodicRescanBroadcastReceiverWithNullContext() {
+        // Given
+        val receiver = ManualEventAlarmPeriodicRescanBroadcastReceiver()
+        val intent = Intent()
+
+        // When - null context
+        receiver.onReceive(null, intent)
+
+        // Then - should not call CalendarMonitor
+        verify(exactly = 0) { mockCalendarMonitor.onPeriodicRescanBroadcast(any(), any()) }
+    }
+
+    // === RescheduleConfirmationsBroadcastReceiver tests ===
+
+    @Test
+    fun testRescheduleConfirmationsBroadcastReceiverCallsApplicationController() {
+        // Given
+        val receiver = RescheduleConfirmationsBroadcastReceiver()
+        val intent = Intent().apply {
+            putExtra("reschedule_confirmations", "test_value")
+        }
+
+        // When
+        receiver.onReceive(context, intent)
+
+        // Then
+        verify(exactly = 1) { ApplicationController.onReceivedRescheduleConfirmations(context, "test_value") }
+    }
+
+    @Test
+    fun testRescheduleConfirmationsBroadcastReceiverWithNullContext() {
+        // Given
+        val receiver = RescheduleConfirmationsBroadcastReceiver()
+        val intent = Intent().apply {
+            putExtra("reschedule_confirmations", "test_value")
+        }
+
+        // When - null context
+        receiver.onReceive(null, intent)
+
+        // Then - should not call ApplicationController
+        verify(exactly = 0) { ApplicationController.onReceivedRescheduleConfirmations(any(), any()) }
+    }
+
+    @Test
+    fun testRescheduleConfirmationsBroadcastReceiverWithNullIntent() {
+        // Given
+        val receiver = RescheduleConfirmationsBroadcastReceiver()
+
+        // When - null intent
+        receiver.onReceive(context, null)
+
+        // Then - should not call ApplicationController
+        verify(exactly = 0) { ApplicationController.onReceivedRescheduleConfirmations(any(), any()) }
+    }
+
+    @Test
+    fun testRescheduleConfirmationsBroadcastReceiverWithMissingExtra() {
+        // Given
+        val receiver = RescheduleConfirmationsBroadcastReceiver()
+        val intent = Intent() // No extra
+
+        // When
+        receiver.onReceive(context, intent)
+
+        // Then - should not call ApplicationController (value is null)
+        verify(exactly = 0) { ApplicationController.onReceivedRescheduleConfirmations(any(), any()) }
     }
 }
 
