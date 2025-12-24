d514a44dbbdb2c16a9153571048a279914455b79
William Harris <wharris@upscalews.com>
2025-12-24 21:50:32 +0000

    fix: un wanted plugin drop

--- babel.config.js
+++ babel.config.js
@@ -29,6 +29,7 @@ module.exports = function (api) {
         },
       ],
       'react-native-worklets/plugin',
+      'react-native-reanimated/plugin', // Reanimated plugin must be listed LAST
     ],
   };
 };
