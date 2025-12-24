f74c096311aefc8ce8c0266f84e0f7488cab7a0a
William Harris <wharris@upscalews.com>
2025-12-24 22:50:44 +0000

    fix: turns out we only need reanimated
    it pulls in worklets

--- babel.config.js
+++ babel.config.js
@@ -28,8 +28,7 @@ module.exports = function (api) {
           },
         },
       ],
-      'react-native-worklets/plugin',
-      'react-native-reanimated/plugin', // Reanimated plugin must be listed LAST
+      'react-native-reanimated/plugin', // Reanimated plugin must be listed LAST (includes worklets)
     ],
   };
 };
